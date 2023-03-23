a122f23b8cf90a2062d572016f0b65997cd3d210
ivVane <iv.vane@gmail.com>
2023-03-23 01:00:41 +0100

    Starting point of the codelab.

---
+++ app/src/main/java/com/example/kotlinwordsapp/WordListFragment.kt
@@ -0,0 +1,60 @@
+package com.example.kotlinwordsapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.kotlinwordsapp.databinding.FragmentWordListBinding
+
+/**
+ * Displays a [RecyclerView] of words with search buttons to look them up.
+ */
+class WordListFragment : Fragment(R.layout.fragment_word_list) {
+
+    /**
+     * Provides global access to these variables from anywhere in the app
+     * via DetailListFragment.<variable> without needing to create
+     * a DetailListFragment instance.
+     */
+    companion object {
+        val LETTER = "letter"
+        val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+
+    private var _binding: FragmentWordListBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private lateinit var letterId: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Retrieve the LETTER from the Fragment arguments
+        arguments?.let {
+            letterId = it.getString(LETTER).toString()
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = WordAdapter(letterId, requireContext())
+
+        // Adds a [DividerItemDecoration] between item
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        )
+    }
+
+    /**
+     * Frees the binding object when the Fragment is destroyed.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
